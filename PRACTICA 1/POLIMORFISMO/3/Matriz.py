8c418e7f5800496d69196fe53facd5b3428ac503
Aneru23 <ailimorit@fcpn.edu.bo>
2025-10-05 00:43:07 -0400

    Matriz.py

--- PRACTICA 1/POLIMORFISMO/3/Matriz.py
+++ PRACTICA 1/POLIMORFISMO/3/Matriz.py
@@ -1,31 +1,46 @@
-class Animal:
-    def __init__(self, nombre, edad):
-        self.nombre = nombre
-        self.edad = edad
+class Matriz:
+    def __init__(self, matriz=None):
+        if matriz is None:
+            self.matriz = [[1 if i == j else 0 for j in range(10)] for i in range(10)]
+        else:
+            self.matriz = matriz
 
-    def desplazarse(self):
-        print("El animal se desplaza de alguna forma.")
+    @classmethod
+    def crear_vacia(cls):
+        matriz_vacia = [[0 for _ in range(10)] for _ in range(10)]
+        return cls(matriz_vacia)
 
-class Leon(Animal):
-    def desplazarse(self):
-        print(f"{self.nombre} camina majestuosamente por la sabana.")
+    def sumar(self, otra):
+        resultado = [[self.matriz[i][j] + otra.matriz[i][j] for j in range(10)] for i in range(10)]
+        return Matriz(resultado)
 
+    def restar(self, otra):
+        resultado = [[self.matriz[i][j] - otra.matriz[i][j] for j in range(10)] for i in range(10)]
+        return Matriz(resultado)
 
-class Pinguino(Animal):
-    def desplazarse(self):
-        print(f"{self.nombre} nada y se desliza sobre el hielo.")
+    def igual(self, otra):
+        for i in range(10):
+            for j in range(10):
+                if self.matriz[i][j] != otra.matriz[i][j]:
+                    return False
+        return True
 
+    # Mostrar matriz
+    def mostrar(self):
+        for fila in self.matriz:
+            print(fila)
 
-class Canguro(Animal):
-    def desplazarse(self):
-        print(f"{self.nombre} salta grandes distancias.")
+m1 = Matriz()            
+m2 = Matriz.crear_vacia()    
+m3 = m1.sumar(m2)            
 
+print("Matriz 1:")
+m1.mostrar()
 
-animales = [
-    Leon("Simba", 5),
-    Pinguino("Pingo", 3),
-    Canguro("Jack", 4)
-]
+print("\nMatriz 2:")
+m2.mostrar()
 
-for a in animales:
-    a.desplazarse()
+print("\nSuma de matrices:")
+m3.mostrar()
+
+print("\n¿m1 igual a m3?:", m1.igual(m3))
